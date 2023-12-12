214f19c1f888713ac7cdbd5b84a63352d777487b
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-12-11 16:33:41 -0800

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch17 Using Machine Learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch17 Using Machine Learning
+//
+//  Created by Student Account on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch17 Using Machine Learning/In_Class_Activity_Pro_IPhone_Ch17_Using_Machine_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch17_Using_Machine_LearningApp.swift
+//  In-Class Activity Pro IPhone Ch17 Using Machine Learning
+//
+//  Created by Student Account on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch17_Using_Machine_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
